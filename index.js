ab4f9c02dfa935ac36c55106fcf4678471f64ee3
masrulmollah <54163010+masrulmollah@users.noreply.github.com>
2023-11-28 00:02:12 +0600

    Update index.js

--- index.js
+++ index.js
@@ -4,13 +4,11 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-// Set up middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Multer configuration for file upload
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'uploads'); // Set your destination folder for uploaded resumes
+    cb(null, 'uploads');
   },
   filename: function (req, file, cb) {
     cb(null, Date.now() + '-' + file.originalname);
@@ -19,18 +17,15 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 
-// Serve HTML file for uploading resumes
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-// Handle resume upload
 app.post('/upload', upload.single('resume'), (req, res) => {
   res.send('Resume uploaded successfully!');
 });
 
-// Handle comments
-let comments = []; // Store comments in memory for this example
+let comments = [];
 
 app.post('/comment', (req, res) => {
   const comment = req.body.comment;
@@ -38,12 +33,10 @@ app.post('/comment', (req, res) => {
   res.send('Comment added successfully!');
 });
 
-// Retrieve comments
 app.get('/comments', (req, res) => {
   res.json(comments);
 });
 
-// Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
